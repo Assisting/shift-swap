cd602b934d3baf2ac4998be77ff89fd6cf0ebfdc
linxcat <clavoy_comp@hotmail.com>
2014-10-02 15:22:15 -0600

    Added controller support for login
    WOOOOOOOOOO!!!!!

---
+++ Request.java
@@ -0,0 +1,90 @@
+/*
+*  Connor Lavoy
+*  cbl013
+*  11118147
+*  October 2, 2014
+*/
+
+public class Request
+{
+
+	public enum RequestType { TAKE, GIVE, TRADE, LOGIN }
+
+	private String sender;
+	private String approver;
+	private bool approved;
+	private String recipient;
+	//private Shift shifts;
+	private String message;
+
+	private RequestType mode;
+
+	//creates a shift-swap request with a certain 'to' and 'from' line
+	public Request(String sender, String recipient, /*Shift shiftset, */String message, RequestType mode)
+	{
+		this.sender = sender;
+		this.recipient = recipient;
+		this.message = message;
+		this.mode = mode;
+	}
+
+//-----Getters and Setters----------------------------------------
+
+	public void setApprover(String name)
+	{
+		approver = name;
+	}
+
+	public void setApproved(bool value)
+	{
+		approved = value;
+	}
+
+//	public void setShifts (Shift shifts)
+//	{
+//	}
+
+	public void setRecipient (String name)
+	{
+		recipient = name;
+	}
+
+	public String getSender()
+	{
+		return sender;
+	}
+
+	public String getApprover()
+	{
+		return approver;
+	}
+
+	public bool isApproved()
+	{
+		return approved;
+	}
+
+	public String getRecipient()
+	{
+		return recipient;
+	}
+
+//	public Shift getShifts()
+//	{
+//	}
+
+	public RequestType getMode()
+	{
+		return mode;
+	}
+
+	public String getMesage()
+	{
+		return message;
+	}
+
+	public void setMessage(String message)
+	{
+		this.message = message;
+	}
+}
