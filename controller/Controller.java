02643bcbf1067ed420cec10a563bcd69c8434990
magnusandy <magnusandy@gmail.com>
2014-10-19 23:18:57 -0600

    added more functins
    tot the controller  more query generators

--- controller/Controller.java
+++ controller/Controller.java
@@ -143,65 +143,102 @@ public class Controller {
          * @param wage set to -1 if not specified
          * @return a custom string for the update query given the parameters 
          */
-        private String updateEmployeeQuery(String firstName, String lastName, int accesslevel, String loginID, String password, String email, float wage)
-        {
+        private String updateEmployeeQuery(String firstName, String lastName, int accesslevel, String loginID, String password, String email, float wage){
             boolean needComma = false;
             String ret = "UPDATE employees SET ";
-            if(firstName != null)
-            {
+            if(firstName != null){
                 ret = ret+ "empfirstname = '"+ firstName+"'";
                 needComma = true;
             }
-            if(lastName != null)
-            {
-                if(needComma == true)
-                {
+            if(lastName != null){
+                if(needComma == true){
                         ret = ret + ", ";
                 }
                 ret = ret+ "emplastname = '"+ lastName+"' ";
                 needComma = true;
             }
-            if(accesslevel != -1)
-            {
-                if(needComma == true)
-                {
+            if(accesslevel != -1){
+                if(needComma == true){
                         ret = ret + ", ";
                 }
                 ret = ret+ "empaccesslevel = '"+ accesslevel+"' ";
                 needComma = true;
             }
-            if(password != null)
-            {
-                if(needComma == true)
-                {
+            if(password != null){
+                if(needComma == true){
                         ret = ret + ", ";
                 }
                 ret = ret+ "emppassword = '"+ password+"' ";
                 needComma = true;
             }
-            if(email != null)
-            {
-                if(needComma == true)
-                {
+            if(email != null){
+                if(needComma == true) {
                         ret = ret + ", ";
                 }
                 ret = ret+ "empemail = '"+ email+"' ";
                 needComma = true;
             }
-            if(wage != -1)
-            {
-                if(needComma == true)
-                {
+            if(wage != -1){
+                if(needComma == true){
                         ret = ret + ", ";
                 }
                 ret = ret+ "empwage = '"+ wage+"' ";
                 needComma = true;
-            }
-            
+            }        
             ret = ret + " WHERE emplogin = '" + loginID +"'";
-            
-            
             return ret;
+        }
+        
+        /**
+         * Generate Query to insert into the bossmanager table, employee is the LoginID
+         * of the employee and manager is the loginID of that persons manager
+         * @param employee LoginID of the employee
+         * @param manager LoginID of the employees manager
+         * @return Query
+         */
+        private String newManagerQuery(String employee, String manager)
+        {
+             return "INSERT INTO bossmanager (employee, manager)"
+                    + " VALUES ('" + employee + "', "
+                    + "'" + manager + "')";
+        }
+        
+        /**
+         * Generate Query to update a employees manager
+         * @param employee employee whos manager needs changing
+         * @param newManager new managers LoginID
+         * @return  the Query
+         */
+        private String updateManagerQuery(String employee, String newManager)
+        {
+            return "UPDATE bossmanager "
+                    + "SET manager = '" + newManager +"' "
+                    + "WHERE employee = '" + employee + "' ";
+        }
+        
+        /**
+         * Generate Query to insert a new message into the inbox table
+         * @param sender LoginId of who sent it
+         * @param reciever LoginID of who should recieve it
+         * @param message  textual part of the message
+         * @return the query
+         */
+        private String newInboxQuery(String sender, String reciever, String message )
+        {
+            return "INSERT INTO employeeinbox (mssgsender, mssgreciever, mssgtext)"
+                    + "VALUES("
+                    + "'" + sender + "', "
+                    + "'" + reciever + "', "
+                    + "'" + message + "' "
+                    + ")";
+        }
+        
+        private String getEmployeeInbox(String LoginID)
+        {
+            return "SELECT mssgreciever, mssgsender, mssgtext, mssgsendtime "
+                    + "FROM employeeinbox "
+                    + "WHERE mssgreciever = '" + LoginID + "' "
+                    + "ORDER BY mssgsendtime";
         }
      
         
@@ -213,5 +250,9 @@ public class Controller {
             System.out.println(c.getEmployeeShiftInfo("testUsername"));
             System.out.println(c.newEmployeeQuery("Elmer", "Fudd", 1, "eFudd", "wabbit", "Fudd@mail.com", (float) 53.232));
             System.out.println(c.updateEmployeeQuery(null, "buster", 2, "eFudd", "jack", null, -1));
-	    }  
+            System.out.println(c.newManagerQuery("testUsername", "testManager"));
+            System.out.println(c.updateManagerQuery("magnusandy", "oneTrueGod"));
+            System.out.println(c.newInboxQuery("magnusandy", "oneTrueGod", "Yoooo dawg lets do this"));
+            System.out.println(c.getEmployeeInbox("oneTrueGod"));
+        }  
 }
