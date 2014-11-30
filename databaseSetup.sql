cdf44fa1c5e7baca784e0cf57628a2f7b94c9ca4
knames <okenso@gmail.com>
2014-11-29 20:40:08 -0600

    sql file to create empty database
    @andrew: fix my defaults and add domains i missed and done!

--- databaseSetup.sql
+++ databaseSetup.sql
@@ -1,3 +1,8 @@
+BEGIN;
+
+-- needs a few domains i missed, and a run incase i mispelled
+-- also needs the defaults (forgot how to do)
+
 DROP DOMAIN employeeLogin CASCADE;
 DROP DOMAIN employeeName CASCADE;
 
@@ -16,7 +21,7 @@ CREATE TABLE employees (
 	empLogin employeeLogin NOT NULL, -- depends on how you want to do login, either by empNum or a username
 	empPassword bytea NOT NULL,  -- I think this will be an hash or some sort, IM FILLING WITH PLAIN TEXT to start
 	empEmail varchar(60) NOT NULL,
-	empWage decimal NOT NULL,
+	empWage numeric NOT NULL,
 	PRIMARY KEY (empLogin)
 );
 
@@ -37,165 +42,47 @@ CREATE TABLE employeeShifts (
 	PRIMARY KEY (shiftEmployeeLogin, shiftStartTime, shiftEndTime)
 );
 
+CREATE TABLE employeeinbox (
+	mssgreciever employeelogin NOT NULL,
+	mssgsender employeelogin NOT NULL,
+	mssgsendtime timestamp NOT NULL, -- how do i default to now()
+	mssgtext text NOT NULL,
+	mssgisread boolean WITH OPTIONS DEFAULT f, -- how do i defaut again
+	PRIMARY KEY (mssgreciever, mssgsender, mssgsendtime)
+);
 
---INSERT Some demo values
-/*--Commenting these Inserts out because we have better sample stuff now
-INSERT 
-	INTO employees (
-		empFirstName,
-		empLastName,
-		empAccessLevel,
-		empLogin,
-		empPassword,
-		empEmail,
-		empWage)
-	VALUES (
-		'Andrew',
-		'Magnus',
-		1,
-		'magnusandy',
-		'[B@677327b6',
-		'testemail@test.com',
-		10)
-;
+CREATE TABLE giveshifts (
+	giverlogin employeelogin NOT NULL,
+	givershiftstart timestamp NOT NULL,
+	givershiftend timestamp NOT NULL,
+	PRIMARY KEY (giverlogin, givershiftstart, givershiftend),
+	FOREIGN KEY (giverlogin, givershiftstart, givershiftend) REFERENCES employeeshifts(shiftemployeelogin, shiftstarttime, shiftendtime)
+);
 
-INSERT 
-	INTO employees (
-		empFirstName,
-		empLastName,
-		empAccessLevel,
-		empLogin,
-		empPassword,
-		empEmail,
-		empWage)
-	VALUES (
-		'Rick',
-		'James',
-		2,
-		'rickjames',
-		'[B@14ae5a5',
-		'rickjames@email.com',
-		10)
-;
+CREATE TABLE managerapproval (
+	ma_approval boolean not null,
+	ma_manager employeelogin not null,
+	PRIMARY KEY (ma_approval, ma_manager),
+	FOREIGN KEY (ma_manager) references employeelogin (emplogin)
+);
 
-INSERT 
-	INTO employees (
-		empFirstName,
-		empLastName,
-		empAccessLevel,
-		empLogin,
-		empPassword,
-		empEmail,
-		empWage)
-	VALUES (
-		'Nic',
-		'Cage',
-		3,
-		'oneTrueGod',
-		'[B@7f31245a',
-		'nicCage@email.com',
-		10)
-;
-
-INSERT 
-	INTO employeeShifts (
-			shiftEmployeeLogin,
-			shiftStartTime,
-			shiftEndTime)
-	VALUES (
-		'magnusandy',
-		'2014-10-13 10:15',
-		'2014-10-13 18:30')
-;
-
-INSERT 
-	INTO employeeShifts (
-			shiftEmployeeLogin,
-			shiftStartTime,
-			shiftEndTime)
-	VALUES (
-		'rickjames',
-		'2014-10-13 10:15',
-		'2014-10-13 18:30')
-;
-
-INSERT 
-	INTO employeeShifts (
-			shiftEmployeeLogin,
-			shiftStartTime,
-			shiftEndTime)
-	VALUES (
-		'magnusandy',
-		'2014-10-14 10:15',
-		'2014-10-14 18:30')
-;
-
-INSERT 
-	INTO employeeShifts (
-			shiftEmployeeLogin,
-			shiftStartTime,
-			shiftEndTime)
-	VALUES (
-		'rickjames',
-		'2014-10-13 19:15',
-		'2014-10-13 21:30')
-;
-
-INSERT 
-	INTO employeeShifts (
-			shiftEmployeeLogin,
-			shiftStartTime,
-			shiftEndTime)
-	VALUES (
-		'magnusandy',
-		'2014-10-20 11:15',
-		'2014-10-13 19:30')
-;
-*/
--- VIEWS
-CREATE VIEW vw_sample_all_employee_week
-	AS SELECT
-		CONCAT (empLastName, ', ',empFirstName) AS "Employee",
-		shiftStartTime AS "Start Time",
-		shiftEndTime AS "End Time",
-		shiftEndTime - shiftStartTime AS "Duration"
-	FROM
-		employees,
-		employeeShifts
-	WHERE
-		empLogin = shiftEmployeeLogin AND
-		shiftStartTime >= '2014-10-12 00:00' AND
-		shiftStartTime < '2014-10-20 00:00'
-	ORDER BY
-		shiftStartTime,
-		empLastName
-;
-
-
-CREATE VIEW login
-	AS SELECT
-		empLogin,
-		empPassword
-	FROM
-		employees
-;
-
-CREATE VIEW full_employee_info 
-	AS SELECT
-		empFirstName,
-		empLastName,
-		empAccessLevel,
-		empLogin,
-		empPassword,
-		empEmail,
-		manager AS empManager
-	FROM
-		employees,
-		BossManager
-	WHERE
-		empLogin = employee
-;
+CREATE TABLE shifttransaction (
+	transactionid integer not null, -- defualt nextval how do this
+	initlogin employeelogin not null,
+	initshiftstart timestamp,
+	initshiftend timestamp,
+	finallogin employeelogin not null,
+	finalshiftstart timestamp not null,
+	finalshiftend timestamp not null,
+	transactiontype trantype not null,
+	finalsign signoff, -- default false
+	finalmanagerlogin employeelogin not null,
+	initmanagerlogin employeelogin not null,
+	initmanagersign boolean, --default false
+	finaltmanagersign boolean, -- default faslse
+	PRIMARY KEY (transactionid)
+);
 
 
 
-
+ROLLBACK;
